d215096a35d4fd9dbd122baf1e7fcb56bb8594d9
GustavoJFR <gustavo.2149337@discente.uemg.br>
2023-06-25 23:47:19 -0300

    25/06/2023

---
+++ script2.js
@@ -0,0 +1,6 @@
+const meuIcone = document.getElementById('botao');
+
+meuIcone.addEventListener('click', function() {
+  // Ação a ser executada quando o ícone for clicado
+  console.log('Ícone clicado!');
+});
