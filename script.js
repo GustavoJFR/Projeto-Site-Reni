7366d00198cb2af55514c07bc6730810385f8caf
GustavoJFR <gustavo.2149337@discente.uemg.br>
2023-07-08 21:59:32 -0300

    08/07/2023

---
+++ script.js
@@ -0,0 +1,7 @@
+function openModal() {
+    document.querySelector('.modal').style.display = 'block';
+  }
+  
+  function closeModal() {
+    document.querySelector('.modal').style.display = 'none';
+  }
