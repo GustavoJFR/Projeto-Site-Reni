e8f2f2ac95507d3863624ad0a31433ec6b81e457
GustavoJFR <gustavo.2149337@discente.uemg.br>
2023-07-08 21:23:53 -0300

    08/07/2023

--- script.js
+++
@@ -1,6 +0,0 @@
- // Função para ser chamada quando o botão for clicado
-    function botaoClicado() {
-        window.location.href="login.html"
-    }
-    var botao = document.getElementById("botao");
-    botao.onclick = botaoClicado;
