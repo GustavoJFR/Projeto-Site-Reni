ca9e497d0297db4190dde1ffff12920234712d73
GustavoJFR <gustavo.2149337@discente.uemg.br>
2023-07-08 23:05:50 -0300

    1213132

--- script.js
+++
@@ -1,7 +0,0 @@
-function openModal() {
-    document.querySelector('.modal').style.display = 'block';
-  }
-  
-  function closeModal() {
-    document.querySelector('.modal').style.display = 'none';
-  }
